e9b5b63d305e0bb258c860dff4edcdcc1116a748
andrey <a.gritsik@gmail.com>
2017-02-12 20:53:44 +0200

    real-time chart

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 var express = require('express');
 var app = express();
 
-// ws specific
+
+// Static pages
+app.use(express.static('public'));
+
+
+// REST end-points
+app.get('/orders', (req, res) => {
+  let data = [];
+  let time = (new Date()).getTime();
+
+  for (let i = -19; i <= 0; i += 1) {
+    data.push({
+      x: time + i * 1000,
+      y: Math.random() * 100
+    });
+  }
+  res.json(data);
+});
+
+
+// Web-sockets
 const http = require('http');
 const WebSocket = require('ws');
-
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 wss.on('connection', function connection(ws) {
   console.log('new client connected');
 
+  let k = 0;
   const interval = setInterval(() => {
-    ws.send("ping");
+    let value = Math.random() * 100 + (25 * k++);
+    let p = [(new Date()).getTime(), value];
+    ws.send(JSON.stringify(p));
   }, 1000);
 
   ws.on('close', () => clearInterval(interval));
-
-  ws.on('message', (message) => console.log('received: %s', message));
-
 });
 
-app.use(express.static('public'));
+
 server.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
